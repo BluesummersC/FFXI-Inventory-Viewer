f7dce50c511ec52bc819185b3e092bbf1a2c64d0
BluesummersC <63878937+BluesummersC@users.noreply.github.com>
2024-12-14 16:08:53 -1000

    Add files via upload

--- addon/armory/armory.lua
+++ addon/armory/armory.lua
@@ -1,6 +1,29 @@
 --[[
-Author: Bluesummers
-Date: 12/12/2024
+Copyright © 2024, Caleb Bluesummers
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of armory nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL Caleb Bluesummers BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 ]]
 
@@ -71,6 +94,7 @@ local function sendFindallFile(passkey)
         return nil
     end
 
+    print("Processing "..player_name..".lua".."...")
     -- Since FindAll outputs a Lua table, we can directly rebuild it here.
     local fileT = dofile(file)
     -- spaces in keys plays hell on the jquery table, go ahead and remove it here
@@ -112,7 +136,7 @@ local function sendFindallFile(passkey)
         return nil
     end
 
-    print("Processing...")
+    print("Sending inventory data to server...")
     -- Call https in order to get the ssl wrapper
     local res = nil
     local code = nil
@@ -162,9 +186,7 @@ local handle_command = function(...)
         local passkey = ""
 
         -- convert command line params (SJIS) to UTF-8
-        for i, elm in ipairs(params) do
-            params[i] = windower.from_shift_jis(elm)
-        end
+        params:map(windower.from_shift_jis)
 
         for _, param in ipairs(params) do
             if S{'--debug', '-d'}:contains(param) then
